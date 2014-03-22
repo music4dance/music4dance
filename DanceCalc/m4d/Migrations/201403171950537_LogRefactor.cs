622a01da8fdaf91a741b7f70070e4bdfea9385b1
David W Gray <davidgray@outlook.com>
2014-03-22 04:36:18 +0000

    Checkin null logrefactor

---
+++ DanceCalc/m4d/Migrations/201403171950537_LogRefactor.cs
@@ -0,0 +1,16 @@
+namespace m4d.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class LogRefactor : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
