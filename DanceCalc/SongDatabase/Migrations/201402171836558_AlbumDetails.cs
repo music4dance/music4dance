aeda05a518bf384b8017425f2b4f5efe22dc413c
David W Gray <davidgray@outlook.com>
2014-04-16 22:48:56 +0000

    Pull in changes that are in dancecalc but have been filtered out of solution

---
+++ DanceCalc/SongDatabase/Migrations/201402171836558_AlbumDetails.cs
@@ -0,0 +1,16 @@
+namespace SongDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AlbumDetails : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
