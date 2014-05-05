17e88057c214a3cfc05f872dec1e4358850be0ab
David W Gray <davidgray@outlook.com>
2014-05-05 21:22:44 +0000

    Refactor oauth migrations down to the base migrations to avoid issues with SQL azure's clustered index constraints

--- DanceCalc/m4d/Migrations/201404102258062_OAuth.cs
+++ DanceCalc/m4d/Migrations/201404102258062_OAuth.cs
@@ -6,49 +6,52 @@ namespace m4d.Migrations
     
     public partial class OAuth : DbMigration
     {
+        // I Refactored OAuth into Base because index creation is messes up wrt SQL Azure's
+        //  clustered index constraints - I believe this is because there is data in the
+        //  table when the index gets reworked.
         public override void Up()
         {
             Trace.WriteLine("Entering DBMigration:OAuth - Up");
-            RenameColumn(table: "dbo.AspNetUserClaims", name: "User_Id", newName: "UserId");
-            RenameIndex(table: "dbo.AspNetUserClaims", name: "IX_User_Id", newName: "IX_UserId");
-            DropPrimaryKey("dbo.AspNetUserLogins");
-            AddColumn("dbo.AspNetUsers", "Email", c => c.String(maxLength: 256));
-            AddColumn("dbo.AspNetUsers", "EmailConfirmed", c => c.Boolean(nullable: false));
-            AddColumn("dbo.AspNetUsers", "PhoneNumber", c => c.String());
-            AddColumn("dbo.AspNetUsers", "PhoneNumberConfirmed", c => c.Boolean(nullable: false));
-            AddColumn("dbo.AspNetUsers", "TwoFactorEnabled", c => c.Boolean(nullable: false));
-            AddColumn("dbo.AspNetUsers", "LockoutEndDateUtc", c => c.DateTime());
-            AddColumn("dbo.AspNetUsers", "LockoutEnabled", c => c.Boolean(nullable: false));
-            AddColumn("dbo.AspNetUsers", "AccessFailedCount", c => c.Int(nullable: false));
-            AlterColumn("dbo.AspNetUsers", "UserName", c => c.String(nullable: false, maxLength: 256));
-            AlterColumn("dbo.AspNetRoles", "Name", c => c.String(nullable: false, maxLength: 256));
-            AddPrimaryKey("dbo.AspNetUserLogins", new[] { "LoginProvider", "ProviderKey", "UserId" });
-            CreateIndex("dbo.AspNetUsers", "UserName", unique: true, name: "UserNameIndex");
-            CreateIndex("dbo.AspNetRoles", "Name", unique: true, name: "RoleNameIndex");
-            DropColumn("dbo.AspNetUsers", "Discriminator");
+            //RenameColumn(table: "dbo.AspNetUserClaims", name: "User_Id", newName: "UserId");
+            //RenameIndex(table: "dbo.AspNetUserClaims", name: "IX_User_Id", newName: "IX_UserId");
+            //DropPrimaryKey("dbo.AspNetUserLogins");
+            //AddColumn("dbo.AspNetUsers", "Email", c => c.String(maxLength: 256));
+            //AddColumn("dbo.AspNetUsers", "EmailConfirmed", c => c.Boolean(nullable: false));
+            //AddColumn("dbo.AspNetUsers", "PhoneNumber", c => c.String());
+            //AddColumn("dbo.AspNetUsers", "PhoneNumberConfirmed", c => c.Boolean(nullable: false));
+            //AddColumn("dbo.AspNetUsers", "TwoFactorEnabled", c => c.Boolean(nullable: false));
+            //AddColumn("dbo.AspNetUsers", "LockoutEndDateUtc", c => c.DateTime());
+            //AddColumn("dbo.AspNetUsers", "LockoutEnabled", c => c.Boolean(nullable: false));
+            //AddColumn("dbo.AspNetUsers", "AccessFailedCount", c => c.Int(nullable: false));
+            //AlterColumn("dbo.AspNetUsers", "UserName", c => c.String(nullable: false, maxLength: 256));
+            //AlterColumn("dbo.AspNetRoles", "Name", c => c.String(nullable: false, maxLength: 256));
+            //AddPrimaryKey("dbo.AspNetUserLogins", new[] { "LoginProvider", "ProviderKey", "UserId" });
+            //CreateIndex("dbo.AspNetUsers", "UserName", unique: true, name: "UserNameIndex");
+            //CreateIndex("dbo.AspNetRoles", "Name", unique: true, name: "RoleNameIndex");
+            //DropColumn("dbo.AspNetUsers", "Discriminator");
             Trace.WriteLine("Exiting DBMigration:OAuth - Up");
         }
         
         public override void Down()
         {
             Trace.WriteLine("Entering DBMigration:OAuth - Down");
-            AddColumn("dbo.AspNetUsers", "Discriminator", c => c.String(nullable: false, maxLength: 128));
-            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
-            DropIndex("dbo.AspNetUsers", "UserNameIndex");
-            DropPrimaryKey("dbo.AspNetUserLogins");
-            AlterColumn("dbo.AspNetRoles", "Name", c => c.String(nullable: false));
-            AlterColumn("dbo.AspNetUsers", "UserName", c => c.String());
-            DropColumn("dbo.AspNetUsers", "AccessFailedCount");
-            DropColumn("dbo.AspNetUsers", "LockoutEnabled");
-            DropColumn("dbo.AspNetUsers", "LockoutEndDateUtc");
-            DropColumn("dbo.AspNetUsers", "TwoFactorEnabled");
-            DropColumn("dbo.AspNetUsers", "PhoneNumberConfirmed");
-            DropColumn("dbo.AspNetUsers", "PhoneNumber");
-            DropColumn("dbo.AspNetUsers", "EmailConfirmed");
-            DropColumn("dbo.AspNetUsers", "Email");
-            AddPrimaryKey("dbo.AspNetUserLogins", new[] { "UserId", "LoginProvider", "ProviderKey" });
-            RenameIndex(table: "dbo.AspNetUserClaims", name: "IX_UserId", newName: "IX_User_Id");
-            RenameColumn(table: "dbo.AspNetUserClaims", name: "UserId", newName: "User_Id");
+            //AddColumn("dbo.AspNetUsers", "Discriminator", c => c.String(nullable: false, maxLength: 128));
+            //DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            //DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            //DropPrimaryKey("dbo.AspNetUserLogins");
+            //AlterColumn("dbo.AspNetRoles", "Name", c => c.String(nullable: false));
+            //AlterColumn("dbo.AspNetUsers", "UserName", c => c.String());
+            //DropColumn("dbo.AspNetUsers", "AccessFailedCount");
+            //DropColumn("dbo.AspNetUsers", "LockoutEnabled");
+            //DropColumn("dbo.AspNetUsers", "LockoutEndDateUtc");
+            //DropColumn("dbo.AspNetUsers", "TwoFactorEnabled");
+            //DropColumn("dbo.AspNetUsers", "PhoneNumberConfirmed");
+            //DropColumn("dbo.AspNetUsers", "PhoneNumber");
+            //DropColumn("dbo.AspNetUsers", "EmailConfirmed");
+            //DropColumn("dbo.AspNetUsers", "Email");
+            //AddPrimaryKey("dbo.AspNetUserLogins", new[] { "UserId", "LoginProvider", "ProviderKey" });
+            //RenameIndex(table: "dbo.AspNetUserClaims", name: "IX_UserId", newName: "IX_User_Id");
+            //RenameColumn(table: "dbo.AspNetUserClaims", name: "UserId", newName: "User_Id");
             Trace.WriteLine("Exiting DBMigration:OAuth - Down");
         }
     }
