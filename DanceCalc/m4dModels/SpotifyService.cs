804f1a60eab7b66072dd26abf6f83fb333146c15
Music 4 Dance <david@music4dance.net>
2015-01-06 00:07:08 +0000

    Key file for spotify integration...

---
+++ DanceCalc/m4dModels/SpotifyService.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using DanceLibrary;
+using Microsoft.CSharp.RuntimeBinder;
+
+namespace m4dModels
+{
+    class SpotifyService : MusicService
+    {
+        public SpotifyService() :
+            base(ServiceType.Spotify,
+            'S', 
+            "Spotify", 
+            "spotify_client", 
+            "listen on spotify",
+            "http://open.spotify.com/track/{0}", 
+            "https://api.spotify.com/v1/search?q={0}&type=track")
+        {
+        }
+
+        public override IList<ServiceTrack> ParseSearchResults(dynamic results)
+        {
+            var ret = new List<ServiceTrack>();
+
+            var tracks = results.tracks;
+            var items = tracks.items;
+
+            foreach (var track in items)
+            {
+                string imageUrl = null;
+                if (track.images != null)
+                {
+                    var width = int.MaxValue;
+                    foreach (var image in track.images)
+                    {
+                        if (image.width >= width) continue;
+
+                        imageUrl = image.url;
+                        width = image.width;
+                    }
+                }
+
+
+                var artist = (track.artists.Length > 0) ? track.artists[0] : null;
+                var album = track.album;
+
+                // TODO: Genre appears to be broken????
+
+                Trace.WriteLine(string.Format("TrackId={0}", (string)track.id));
+                Trace.WriteLine(string.Format("Name={0}", (string)track.name));
+                Trace.WriteLine(string.Format("Artist={0}", (string)(artist == null ? null : artist.name)));
+                Trace.WriteLine(string.Format("Album={0}", (string)(album == null ? null : album.name)));
+                Trace.WriteLine(string.Format("CollectionId={0}", (string)(album == null ? null : album.id)));
+                Trace.WriteLine(string.Format("ImageUrl={0}", imageUrl));
+//                Trace.WriteLine(string.Format("Genre={0}", (string)((artist == null || artist.genres.Length == 0) ? null : artist.genres[0])));
+                Trace.WriteLine(string.Format("Duration={0}", (string)((track.duration_ms + 500)/1000).ToString()));
+                Trace.WriteLine(string.Format("DiscNumber={0}", (string)track.disc_number.ToString()));
+                Trace.WriteLine(string.Format("TrackNumber={0}", (string)track.track_number.ToString()));
+
+                int trackNum = track.track_number;
+                if (track.disc_number > 1)
+                {
+                    trackNum = (int) (new TrackNumber(trackNum, (int)track.disk_number, null));
+                }
+                var st = new ServiceTrack
+                {
+                    Service = ServiceType.Spotify,
+                    TrackId = track.id,
+                    Name = track.name,
+                    //AltId = altId,
+                    Artist = artist == null ? null : artist.name,
+                    Album = album == null ? null : album.name,
+                    CollectionId = album == null ? null : album.id,
+                    ImageUrl = imageUrl,
+                    //ReleaseDate = track.ReleaseDate,
+                    //Genre = (artist == null || artist.genres.Length == 0) ? null : artist.genres[0],
+                    Duration = (track.duration_ms + 500) / 1000,
+                    TrackNumber = trackNum,
+                };
+
+                ret.Add(st);
+            }
+
+            return ret;
+        }
+    }
+}
