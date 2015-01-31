30c29b7eb2babf61f6ab0762bb65e3ce5ad28c97
Music 4 Dance <david@music4dance.net>
2015-01-31 00:13:28 +0000

    Initial pass at 404 page handling

---
+++ DanceCalc/m4d/Controllers/ErrorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Mvc;
+
+using m4dModels;
+
+namespace m4d.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        [AllowAnonymous]
+        public ActionResult Index(int statusCode = 500, Exception exception = null, bool isAjaxRequest = false)
+        {
+            // If it's not an AJAX request that triggered this action then just retun the view
+            if (!isAjaxRequest)
+            {
+                var model = new ErrorModel { HttpStatusCode = statusCode, Exception = exception };
+
+                Response.StatusCode = statusCode; 
+                Response.TrySkipIisCustomErrors = true;
+
+                return View(model);
+            }
+            else
+            {
+                // Otherwise, if it was an AJAX request, return an anon type with the message from the exception
+                var errorObject = new { message = (exception == null) ? "Really Bad Error" : exception.Message };
+                return Json(errorObject, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
--- DanceCalc/m4d/Global.asax.cs
+++ DanceCalc/m4d/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using m4d.Controllers;
 using m4d.Utilities;
 using m4dModels;
 
@@ -19,6 +21,41 @@ namespace m4d
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             ModelBinders.Binders[typeof(SongDetails)] = new SongBinder();
+        }
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var lastError = Server.GetLastError();
+            Server.ClearError();
+
+            var statusCode = 0;
+
+            // ReSharper disable once ConvertIfStatementToConditionalTernaryExpression
+            if ((lastError != null) && (lastError.GetType() == typeof(HttpException)))
+            {
+                statusCode = ((HttpException)lastError).GetHttpCode();
+            }
+            else
+            {
+                // Not an HTTP related error so this is a problem in our code, set status to
+                // 500 (internal server error)
+                statusCode = 500;
+            }
+
+            var contextWrapper = new HttpContextWrapper(Context);
+
+            var routeData = new RouteData();
+            routeData.Values.Add("controller", "Error");
+            routeData.Values.Add("action", "Index");
+            routeData.Values.Add("statusCode", statusCode);
+            routeData.Values.Add("exception", lastError??new Exception("Something Really Went WRONG!!!"));
+            routeData.Values.Add("isAjaxRequest", contextWrapper.Request.IsAjaxRequest());
+
+            IController controller = new ErrorController();
+
+            var requestContext = new RequestContext(contextWrapper, routeData);
+
+            controller.Execute(requestContext);
+            Response.End();
         }
     }
 }
