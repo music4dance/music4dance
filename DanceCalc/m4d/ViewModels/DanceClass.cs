ffdd0dc6ec992913d2504cc931d06097aa57d8ee
Music 4 Dance <david@music4dance.net>
2015-04-08 01:48:33 +0000

    Initial pass at hompage rework

---
+++ DanceCalc/m4d/ViewModels/DanceClass.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using DanceLibrary;
+
+namespace m4d.ViewModels
+{
+    public class DanceMapping
+    {
+        public DanceMapping(string title = null, string name = null)
+        {
+            Title = title;
+            Name = name ?? DanceObject.SeoFriendly(title);
+        }
+
+        public string Name;
+        public string Title;
+    }
+    public class DanceClass
+    {
+        public string Title;
+        public string Image;
+        public string TopDance;
+
+        public List<DanceMapping> Dances;
+    }
+}
