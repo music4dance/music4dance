20e9486070e5d5ca39a04072c39d19d11b15e4e7
Music 4 Dance <david@music4dance.net>
2015-09-04 22:03:04 +0000

    Fix for edit pages where there is a gap in the album list array

--- DanceCalc/m4d/Scripts/edit.js
+++ DanceCalc/m4d/Scripts/edit.js
@@ -305,6 +305,10 @@ var editor = function () {
         //{'copy': 'PurchaseLinks' }
         ko.mapping.fromJS(data, {}, this);
 
+        if (!self.hasOwnProperty('Name')) {
+            console.Log('FAILED:' + data);
+        }
+
         self.addPurchase = function (track) {
             //var pi = self.PurchasInfo();
             //var r = /[AIX][SA]=[^;]*/g, match;
