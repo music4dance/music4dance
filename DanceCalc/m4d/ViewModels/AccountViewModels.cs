7992b014bcf38abe40a8db902031ea9e94ab8c32
Amanda Eller <amanda_eller@hotmail.com>
2015-09-14 18:41:18 +0000

    Bug 261: I added text at the bottom of the registration page that tells you the requirements needed for creating a password. The text is linked from the Password box by way of an * which will take you to the note as well as indicating that there is such a note
    Bug 266: As an easy first step fix to not send confirmation emails to wrong addresses, I added a Confirm Email box on the registration page. making the new user to type their email twice and then compares to make sure they match. Hopefully will cut down on miss typed addresses so more valid addresses will go through.

--- DanceCalc/m4d/ViewModels/AccountViewModels.cs
+++ DanceCalc/m4d/ViewModels/AccountViewModels.cs
@@ -57,7 +57,7 @@ namespace m4d.ViewModels
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = @"Password")]
+        [Display(Name = @"Password*")]
         public string Password { get; set; }
 
         [Display(Name = @"Remember me?")]
@@ -74,6 +74,12 @@ namespace m4d.ViewModels
         [EmailAddress]
         [Display(Name = @"Email")]
         public string Email { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = @"Confirm Email")]
+        [Compare("Email", ErrorMessage = @"Email address and confirmation email do not match")]
+        public string ConfirmEmail { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = @"The {0} must be at least {2} characters long.", MinimumLength = 6)]
