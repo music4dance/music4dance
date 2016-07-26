535746e692f16c0316d3484950dfc0b7b3bcd03e
David m4d Gray <david@music4dance.net>
2016-07-26 11:37:19 -0700

    swap back to basica

--- m4dModels/SearchServiceInfo.cs
+++ m4dModels/SearchServiceInfo.cs
@@ -70,7 +70,7 @@ namespace m4dModels
             if (env == null)
             {
                 s_useSql = false;
-                return s_defaultId = "basicb";
+                return s_defaultId = "basica";
             }
 
             s_env = env;
