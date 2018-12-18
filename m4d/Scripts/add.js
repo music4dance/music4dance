b1d59e74e4db953643c87d3c94f743910eb49e0b
Music 4 Dance <david@music4dance.net>
2018-12-17 20:10:18 -0800

    Make spotify regex more robust

--- m4d/Scripts/add.js
+++ m4d/Scripts/add.js
@@ -11,7 +11,7 @@
     self.services = [
         { id: 'i', name: 'itunes', rgx: /[0-9]*/i, idm: /[0-9]*/i },
         { id: 'a', name: 'amazon', rgx: /\/(B[a-z0-9]{9})/i, idm: /B[a-z0-9]{9}/i },
-        { id: 's', name: 'spotify', rgx: /([a-z0-9]+)(?:\?si=[a-z0-9 -_]*)?$/i, idm: /[a-z0-9]{22}/i }
+        { id: 's', name: 'spotify', rgx: /([a-z0-9]+)(?:\?.*)?$/i, idm: /[a-z0-9]{22}/i }
     ];
 
     self.serviceFromId = function(id) {
