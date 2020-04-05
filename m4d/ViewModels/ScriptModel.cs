9b2e64c2d4633aaa9cc5e205f8c1afdc9e03f8f2
David W Gray <dwgray67@hotmail.com>
2020-04-05 00:24:15 +0000

    Merged PR 43: CDN and Bundling improvements
    - Add local backups for all CDN bundles and load debug versions in development environment
    - Change bundling so that we load the original files in development mode and only bundle/minify in release

---
+++ m4d/ViewModels/ScriptModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace m4d.ViewModels
+{
+    public class ScriptModel
+    {
+        public bool IncludeTools { get; set; }
+        public string[] DevelopmentFiles { get; set; }
+        public string[] ReleaseFiles { get; set; }
+    }
+}
--- m4d/gulpfile.js
+++ m4d/gulpfile.js
@@ -1,8 +1,9 @@
-﻿/// <binding BeforeBuild='styles' />
+﻿/// <binding BeforeBuild='styles, scripts, fonts' />
 
 var gulp = require("gulp"),
     cleanCss = require("gulp-clean-css"),
-    less = require("gulp-less");
+    less = require("gulp-less"),
+    merge = require('merge2');
 
 gulp.task("styles", function () {
     return gulp.src('Styles/*.less')
@@ -16,3 +17,35 @@ gulp.task('fonts', function () {
         .pipe(gulp.dest('wwwroot/fonts/'));
 });
 
+var deps = {
+    "bootstrap3": {
+        "dist/**/*": ""
+    },
+    "jquery": {
+        "dist/*": ""
+    },
+    "jquery-validation": {
+        "dist/*": ""
+    },
+    "knockout": {
+        "build/output/*": ""
+    },
+    "knockout-mapping": {
+        "dist/*": ""
+    }
+};
+
+gulp.task("scripts", function () {
+
+    var streams = [];
+
+    for (var prop in deps) {
+        console.log("Prepping Scripts for: " + prop);
+        for (var itemProp in deps[prop]) {
+            streams.push(gulp.src("node_modules/" + prop + "/" + itemProp)
+                .pipe(gulp.dest("wwwroot/vendor/" + prop + "/" + deps[prop][itemProp])));
+        }
+    }
+
+    return merge(streams);
+});
