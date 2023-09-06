e82ca0e8fbffe525f05133878ae9e3fcb096d3ef
David W Gray <dwgray67@hotmail.com>
2023-09-06 16:31:09 +0000

    Merged PR 411: Fix SendGrid
    Fix SendGrid

--- m4d/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ m4d/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 
 namespace m4d.Areas.Identity.Pages.Account
--- m4d/Services/EmailSender.cs
+++ m4d/Services/EmailSender.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using m4d.Utilities;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 using System.Threading.Tasks;
@@ -14,6 +16,7 @@ public class AuthMessageSenderOptions
 
 public class EmailSender : IEmailSender
 {
+    protected static readonly ILogger Logger = ApplicationLogging.CreateLogger<EmailSender>();
     public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor)
     {
         Options = optionsAccessor.Value;
@@ -21,27 +24,45 @@ public class EmailSender : IEmailSender
 
     public AuthMessageSenderOptions Options { get; } //set only via Secret Manager
 
-    public Task SendEmailAsync(string email, string subject, string message)
+    public async Task SendEmailAsync(string email, string subject, string message)
     {
-        return Execute(Options.Key, subject, message, email);
+        await Execute(Options.Key, subject, message, email);
     }
 
-    public Task Execute(string apiKey, string subject, string message, string email)
+    public async Task Execute(string apiKey, string subject, string message, string email)
     {
-        var client = new SendGridClient(apiKey);
-        var msg = new SendGridMessage
-        {
-            From = new EmailAddress("support@music4dance.com", Options.User),
-            Subject = subject,
-            PlainTextContent = message,
-            HtmlContent = message
-        };
-        msg.AddTo(new EmailAddress(email));
+        //var client = new SendGridClient(apiKey);
+        //var msg = new SendGridMessage
+        //{
+        //    From = new EmailAddress("info@music4dance.com", Options.User),
+        //    Subject = subject,
+        //    PlainTextContent = message,
+        //    HtmlContent = message
+        //};
+        //msg.AddTo(new EmailAddress(email));
 
-        // Disable click tracking.
-        // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+        //// Disable click tracking.
+        //// See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+        //
+
+
+        var client = new SendGridClient(apiKey);
+        var from = new EmailAddress("support@music4dance.net", "music4dance support");
+        var to = new EmailAddress(email);
+        var plainTextContent = message;
+        var htmlContent = message;
+        var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
         msg.SetClickTracking(false, false);
 
-        return client.SendEmailAsync(msg);
+        var response = await client.SendEmailAsync(msg);
+        if (!response.IsSuccessStatusCode)
+        {
+            Logger.LogError($"Failed to Send Email: {response.StatusCode})");
+            var body = await response.DeserializeResponseBodyAsync();
+            foreach (var item in body)
+            {
+                Logger.LogInformation($"{item.Key}: {item.Value.ToString()}");
+            }
+        }
     }
 }
