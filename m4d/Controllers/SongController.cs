a98e2930e2076478a49ff62faa1ab0d42be5828a
David W. Gray <dwgray67@hotmail.com>
2025-11-14 13:52:07 -0800

    Changes based on code review

--- m4d/Controllers/SongController.cs
+++ m4d/Controllers/SongController.cs
@@ -129,11 +129,11 @@ public class SongController : ContentController
 
         if (playlist == null)
         {
-            return ReturnError(HttpStatusCode.NotFound, 
+            return ReturnError(HttpStatusCode.NotFound,
                 $"Playlist {id} doesn't exist or is empty.");
         }
 
-        var filter = 
+        var filter =
             $"ServiceIds/any(id: search.in(id, '{string.Join(',',playlist.Tracks.Select(t => $"S:{t.TrackId}"))}'))";
         var options = new SearchOptions
         {
@@ -750,11 +750,13 @@ public class SongController : ContentController
 
         try
         {
-            var songMod = SongModifier.Build(properties);
+            // Building this before doing anything else purely for validation, it will
+            //  be rebuilt further down the stack.
+            SongModifier.Build(properties);
         }
-        catch
+        catch (Exception ex)
         {
-            throw new ArgumentException(properties, nameof(properties));
+            throw new ArgumentException($"Invalid properties format: {properties}", nameof(properties), ex);
         }
 
         return BatchAdminExecute(
