5152c31bdecab64106be590067f76ce7da883e0c
David W. Gray <dwgray67@hotmail.com>
2026-08-06 08:56:18 -0700

    Small fix from last PR comments

--- m4d/ClientApp/src/models/DanceDatabase/DanceValidation.ts
+++ m4d/ClientApp/src/models/DanceDatabase/DanceValidation.ts
@@ -1,4 +1,4 @@
-import { jsonMember, jsonObject } from "typedjson";
+import { jsonArrayMember, jsonMember, jsonObject } from "typedjson";
 
 // Mirrors DanceLib/DanceValidation.cs - sanity-check thresholds used to catch Spotify/EchoNest
 // half-time/double-time tempo detection errors. Lives on DanceType (dances.json's top-level
@@ -7,4 +7,5 @@ import { jsonMember, jsonObject } from "typedjson";
 export class DanceValidation {
   @jsonMember(Number) public doubleTempoIfBelow?: number;
   @jsonMember(Number) public halveTempoIfAbove?: number;
+  @jsonArrayMember(String) public flagInvalidMeters?: string[];
 }
